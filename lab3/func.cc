6e9941caae42043075d4cde9b0153afbb483dc10
Pzelazny <pzelazny@stevens.edu>
2021-02-22 12:30:46 -0500

    Added name to function

--- lab3/func.cc
+++ lab3/func.cc
@@ -82,7 +82,7 @@ int max(int a, int b) {}
 // 13
 bool isEven(int a) {}
 
-// 14 
+// 14 Piotr Zelazny
 double perimeter3(int x1, int y1, int x2, int y2, int x3, int y3) {
   double total =0;
   total =((x3-x1)(x3-x1)+(y3-y1)(y3-y1))+((x2-x1)(x2-x1)+(y2-y1)(y2-y1))+((x3-x2)(x3-x2)+(y3-y2)(y3-y2));
