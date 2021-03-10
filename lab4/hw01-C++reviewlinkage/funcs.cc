02df2e79f6a25217125407065db1e8f7dc534ab3
HanyiZhang <734049907@qq.com>
2021-03-10 18:02:54 -0500

    lab6

--- lab4/hw01-C++reviewlinkage/funcs.cc
+++ lab4/hw01-C++reviewlinkage/funcs.cc
@@ -2,3 +2,12 @@
   Write your code here for the required functions
   The makefile builds the code, assuming the functions here match the ones used in main
  */
+#include <cmath>
+double hypotenuse(double a, double b){
+  return sqrt(a * a + b * b);
+}
+
+// compute the average of the two values
+double mean(double a, double b){
+  return (a + b) / 2;
+}
